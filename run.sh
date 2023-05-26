a7205a72715b35668479035c5e86c6e4efc1c782
insanetree <treebian@tutanota.com>
2023-05-26 20:57:40 +0200

    skripta za pokretanje projekta

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mkdir -p "$NS3ROOT/scratch/projekat"
+
+pushd src
+for file in $(ls)
+do
+	cp $file "$NS3ROOT/scratch/projekat/$file"
+done
+popd
+
+pushd h
+for file in $(ls)
+do
+	cp $file "$NS3ROOT/scratch/projekat/$file"
+done
+popd
+
+mkdir -p output
+OUTDIR="$PWD/output"
+
+pushd $NS3ROOT
+./ns3 build
+./ns3 run scratch/projekat.main.cc --cwd=$OUTDIR
+popd
