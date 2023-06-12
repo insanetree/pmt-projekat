d61561d2685fce650b13e1661ee9fcd297acfe90
insanetree <treebian@tutanota.com>
2023-06-12 23:36:36 +0200

    dodeljen ip adrese

--- src/main.cc
+++ src/main.cc
@@ -26,4 +26,18 @@ int main(int argc, char* argv[]){
 	ns3::NetDeviceContainer n2n1 = p2p.Install(nodes.Get(2), nodes.Get(1));
 	ns3::NetDeviceContainer n3n1 = p2p.Install(nodes.Get(3), nodes.Get(1));
 	ns3::NetDeviceContainer n1n0 = p2p.Install(nodes.Get(1), nodes.Get(0));
+	
+	//dodela IP adresa
+	ns3::Ipv4AddressHelper ip;
+	
+	ip.SetBase("10.0.0.0", "255.255.255.0");
+	ns3::Ipv4InterfaceContainer i1i0 = ip.Assign(n1n0);
+
+	ip.SetBase("10.0.1.0", "255.255.255.0");
+	ns3::Ipv4InterfaceContainer i2i1 = ip.Assign(n2n1);
+
+	ip.SetBase("10.0.2.0", "255.255.255.0");
+	ns3::Ipv4InterfaceContainer i3i1 = ip.Assign(n3n1);
+
+	ns3::Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 }
