92c745c03534b56b65bf77e310f3129be47a0cd7
Aleksandar <pa200059d@student.etf.bg.ac.rs>
2023-06-21 21:21:34 +0200

    prvi pregled projekta

--- src/main.cc
+++ src/main.cc
@@ -9,63 +9,82 @@
 #include "ns3/network-module.h"
 #include "ns3/applications-module.h"
 #include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/flow-monitor.h"
+#include "ns3/flow-monitor-helper.h"
+
+#define BaseBandwidth "1Mbps"
+#define IncreasedBandwidth "10Mbps"
+
+
+using namespace ns3;
 
 int main(int argc, char* argv[]){
-	ns3::Config::SetDefault ("ns3::Ipv4GlobalRouting::RespondToInterfaceEvents", ns3::BooleanValue(true));
-	ns3::NodeContainer nodes;
+	Config::SetDefault ("ns3::Ipv4GlobalRouting::RespondToInterfaceEvents", BooleanValue(true));
+	NodeContainer nodes;
 	nodes.Create(4);
 
 	//instalacija steka protokola na cvorovima
-	ns3::InternetStackHelper internet;
+	InternetStackHelper internet;
 	internet.Install(nodes);
 
 	//pravimo linkove
-	ns3::PointToPointHelper p2p;
-	p2p.SetQueue("ns3::DropTailQueue", "MaxSize", ns3::StringValue("50p"));
-	p2p.SetDeviceAttribute("DataRate", ns3::StringValue("100kbps"));
-	p2p.SetChannelAttribute("Delay", ns3::StringValue("5ms"));
-	ns3::NetDeviceContainer n2n1 = p2p.Install(nodes.Get(2), nodes.Get(1));
-	ns3::NetDeviceContainer n3n1 = p2p.Install(nodes.Get(3), nodes.Get(1));
-	p2p.SetDeviceAttribute("DataRate", ns3::StringValue("1Mbps"));
-	ns3::NetDeviceContainer n1n0 = p2p.Install(nodes.Get(1), nodes.Get(0));
+	PointToPointHelper p2p;
+	p2p.SetQueue("ns3::DropTailQueue", "MaxSize", StringValue("100p"));
+	p2p.SetDeviceAttribute("DataRate", StringValue(BaseBandwidth));
+	p2p.SetChannelAttribute("Delay", StringValue("5ms"));
+	NetDeviceContainer n2n1 = p2p.Install(nodes.Get(2), nodes.Get(1));
+	NetDeviceContainer n3n1 = p2p.Install(nodes.Get(3), nodes.Get(1));
+	p2p.SetDeviceAttribute("DataRate", StringValue(IncreasedBandwidth));
+	NetDeviceContainer n1n0 = p2p.Install(nodes.Get(1), nodes.Get(0));
 	
 	//dodela IP adresa
-	ns3::Ipv4AddressHelper ip;
+	Ipv4AddressHelper ip;
 	
 	ip.SetBase("10.0.0.0", "255.255.255.0");
-	ns3::Ipv4InterfaceContainer i1i0 = ip.Assign(n1n0);
+	Ipv4InterfaceContainer i1i0 = ip.Assign(n1n0);
 
 	ip.SetBase("10.0.1.0", "255.255.255.0");
-	ns3::Ipv4InterfaceContainer i2i1 = ip.Assign(n2n1);
+	Ipv4InterfaceContainer i2i1 = ip.Assign(n2n1);
 
 	ip.SetBase("10.0.2.0", "255.255.255.0");
-	ns3::Ipv4InterfaceContainer i3i1 = ip.Assign(n3n1);
+	Ipv4InterfaceContainer i3i1 = ip.Assign(n3n1);
 
-	ns3::Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+	Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
 	//izvor saobracaja
 	uint16_t port = 9;
-	ns3::BulkSendHelper sender("ns3::TcpSocketFactory", ns3::InetSocketAddress(i1i0.GetAddress(1),port));
-	sender.SetAttribute("MaxBytes", ns3::UintegerValue(0));
-	ns3::ApplicationContainer senderApps = sender.Install(ns3::NodeContainer(nodes.Get(2), nodes.Get(3)));
-	senderApps.Start(ns3::Seconds(0.0));
-	senderApps.Stop(ns3::Seconds(20.0));
+//	BulkSendHelper sender("ns3::TcpSocketFactory", InetSocketAddress(i1i0.GetAddress(1),port));
+//	sender.SetAttribute("MaxBytes", UintegerValue(0));
+	OnOffHelper sender("ns3::UdpSocketFactory", InetSocketAddress(i1i0.GetAddress(1),port));
+	sender.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=1]"));
+	sender.SetAttribute("OffTime", StringValue("ns3::ConstantRandomVariable[Constant=0]"));
+	sender.SetAttribute("DataRate", StringValue(BaseBandwidth));
+	sender.SetAttribute("PacketSize", UintegerValue(200));
+	ApplicationContainer senderApps = sender.Install(NodeContainer(nodes.Get(2), nodes.Get(3)));
+	senderApps.Start(Seconds(0.0));
+	senderApps.Stop(Seconds(20.0));
 
 	//prijem saobracaja
-	ns3::PacketSinkHelper sink("ns3::TcpSocketFactory", ns3::InetSocketAddress(ns3::Ipv4Address::GetAny(),port));
-	ns3::ApplicationContainer sinkApps = sink.Install(nodes.Get(0));
-	sinkApps.Start(ns3::Seconds(0.0));
-	sinkApps.Stop(ns3::Seconds(20.0));
+	PacketSinkHelper sink("ns3::UdpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(),port));
+	ApplicationContainer sinkApps = sink.Install(nodes.Get(0));
+	sinkApps.Start(Seconds(0.0));
+	sinkApps.Stop(Seconds(20.0));
 
 
 	//snimanje izvestaja
-	ns3::AsciiTraceHelper ascii;
-	ns3::Ptr<ns3::OutputStreamWrapper> stream = ascii.CreateFileStream("zagusenje.tr");
+	AsciiTraceHelper ascii;
+	Ptr<OutputStreamWrapper> stream = ascii.CreateFileStream("zagusenje.tr");
 	p2p.EnableAsciiAll(stream);
 	p2p.EnablePcapAll("zagusenje");
 
-	ns3::Simulator::Stop(ns3::Seconds(20.0));
-	ns3::Simulator::Run();
-	ns3::Simulator::Destroy();
+	Ptr<FlowMonitor> flowMonitor;
+	FlowMonitorHelper flowMonitorHelper;
+	flowMonitor = flowMonitorHelper.InstallAll();
+
+	Simulator::Stop(Seconds(25.0));
+	Simulator::Run();
+	
+	flowMonitor->SerializeToXmlFile("izlaz.xml",true,true);
+	Simulator::Destroy();
 }
 
